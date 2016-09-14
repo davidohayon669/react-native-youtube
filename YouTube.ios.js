2ed7b1eee0dd7f44fac9740c69eda19c8d1f59cd
Mike Lambert <mlambert@gmail.com>
2016-09-14 02:32:28 -0400

    More style cleanups.

--- YouTube.ios.js
+++ YouTube.ios.js
@@ -55,7 +55,7 @@ export default class YouTube extends Component {
   }
 
   _onChangeState(event: SyntheticEvent) {
-    if(event.nativeEvent.state == 'ended' && this.props.loop) {
+    if (event.nativeEvent.state === 'ended' && this.props.loop) {
       this.seekTo(0);
     }
     return this.props.onChangeState && this.props.onChangeState(event.nativeEvent);
@@ -68,7 +68,7 @@ export default class YouTube extends Component {
   _onError(event: SyntheticEvent) {
     return this.props.onError && this.props.onError(event.nativeEvent);
   }
-  _onProgress(event: SyntheticEvent){
+  _onProgress(event: SyntheticEvent) {
       return this.props.onProgress && this.props.onProgress(event.nativeEvent);
   }
   seekTo(seconds: number){
@@ -83,7 +83,7 @@ export default class YouTube extends Component {
     nativeProps.onYoutubeVideoChangeQuality = this._onChangeQuality.bind(this);
     nativeProps.onYoutubeVideoError = this._onError.bind(this);
     nativeProps.onYoutubeProgress = this._onProgress.bind(this);
-    return <RCTYouTube ref={component => this._root = component} {...nativeProps} />;
+    return <RCTYouTube ref={component => { this._root = component; }} {...nativeProps} />;
   }
 
   componentDidUpdate() {
@@ -105,29 +105,29 @@ export default class YouTube extends Component {
         if (this.props.playsInline) {
           nativeProps.playerParams.playerVars.playsinline = 1;
           delete nativeProps.playsInline;
-        };
+        }
         if (this.props.modestbranding) {
           nativeProps.playerParams.playerVars.modestbranding = 1;
           delete nativeProps.modestbranding;
-        };
+        }
 
-        if (this.props.showinfo!==undefined) {
+        if (this.props.showinfo !== undefined) {
           nativeProps.playerParams.playerVars.showinfo = this.props.showinfo ? 1 : 0;
           delete nativeProps.showinfo;
-        };
-        if (this.props.controls!==undefined) {
+        }
+        if (this.props.controls !== undefined) {
           nativeProps.playerParams.playerVars.controls = this.props.controls;
           delete nativeProps.controls;
-        };
-        if (this.props.origin!==undefined) {
+        }
+        if (this.props.origin !== undefined) {
           nativeProps.playerParams.playerVars.origin = this.props.origin;
           delete nativeProps.origin;
-        };
-        if (this.props.rel!==undefined) {
+        }
+        if (this.props.rel !== undefined) {
           nativeProps.playerParams.playerVars.rel = this.props.rel ? 1 : 0;
           delete nativeProps.rel;
-        };
-      };
+        }
+      }
     } else {
       /*
        * For compatibility issues with an older version where setting both
