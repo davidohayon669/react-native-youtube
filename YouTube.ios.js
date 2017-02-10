5a539c631d36b1e7ac3bef7884f75bd1e6d9e57c
William Candillon <wcandillon@gmail.com>
2017-02-10 10:10:35 +0100

    Update YouTube.ios.js

--- YouTube.ios.js
+++ YouTube.ios.js
@@ -106,70 +106,69 @@ export default class YouTube extends Component {
     enterFullScreen.remove();
     exitFullScreen.remove();
   }
+    render() {
+        var style = [styles.base, this.props.style];
+        var nativeProps = Object.assign({}, this.props);
+        nativeProps.style = style;
 
-  render() {
-    var style = [styles.base, this.props.style];
-    var nativeProps = Object.assign({}, this.props);
-    nativeProps.style = style;
+        /*
+         * Try to use `playerParams` instead of settings `playsInline` and
+         * `videoId` individually.
+         */
+        if (this._exportedProps) {
+            if (this._exportedProps.playerParams) {
+                nativeProps.playerParams = {
+                    videoId: this.props.videoId,
+                };
+                delete nativeProps.videoId;
 
-    /*
-     * Try to use `playerParams` instead of settings `playsInline` and
-     * `videoId` individually.
-     */
-    if (this._exportedProps) {
-      if (this._exportedProps.playerParams) {
-        nativeProps.playerParams = {
-          videoId: this.props.videoId,
-        };
-        delete nativeProps.videoId;
+                nativeProps.playerParams.playerVars = {};
 
-        nativeProps.playerParams.playerVars = {};
+                if (this.props.playsInline) {
+                    nativeProps.playerParams.playerVars.playsinline = 1;
+                    delete nativeProps.playsInline;
+                };
+                if (this.props.modestbranding) {
+                    nativeProps.playerParams.playerVars.modestbranding = 1;
+                    delete nativeProps.modestbranding;
+                };
 
-        if (this.props.playsInline) {
-          nativeProps.playerParams.playerVars.playsinline = 1;
-          delete nativeProps.playsInline;
-        };
-        if (this.props.modestbranding) {
-          nativeProps.playerParams.playerVars.modestbranding = 1;
-          delete nativeProps.modestbranding;
-        };
+                if (this.props.showinfo!==undefined) {
+                    nativeProps.playerParams.playerVars.showinfo = this.props.showinfo ? 1 : 0;
+                    delete nativeProps.showinfo;
+                };
+                if (this.props.controls!==undefined) {
+                    nativeProps.playerParams.playerVars.controls = this.props.controls;
+                    delete nativeProps.controls;
+                };
+                if (this.props.origin!==undefined) {
+                    nativeProps.playerParams.playerVars.origin = this.props.origin;
+                    delete nativeProps.origin;
+                };
+                if (this.props.rel!==undefined) {
+                    nativeProps.playerParams.playerVars.rel = this.props.rel ? 1 : 0;
+                    delete nativeProps.rel;
+                };
+                if (this.props.fs!==undefined) {
+                    nativeProps.playerParams.playerVars.fs = this.props.fs ? 1 : 0;
+                    delete nativeProps.fs;
+                };
+            };
+        } else {
+            /*
+             * For compatibility issues with an older version where setting both
+             * `playsInline` and `videoId` in quick succession would cause the video
+             * to sometimes not play.
+             */
+            delete nativeProps.playsInline;
+        }
 
-        if (this.props.showinfo!==undefined) {
-          nativeProps.playerParams.playerVars.showinfo = this.props.showinfo ? 1 : 0;
-          delete nativeProps.showinfo;
-        };
-        if (this.props.controls!==undefined) {
-          nativeProps.playerParams.playerVars.controls = this.props.controls;
-          delete nativeProps.controls;
-        };
-        if (this.props.origin!==undefined) {
-          nativeProps.playerParams.playerVars.origin = this.props.origin;
-          delete nativeProps.origin;
-        };
-        if (this.props.rel!==undefined) {
-          nativeProps.playerParams.playerVars.rel = this.props.rel ? 1 : 0;
-          delete nativeProps.rel;
-        };
-        if (this.props.fs!==undefined) {
-          nativeProps.playerParams.playerVars.fs = this.props.fs ? 1 : 0;
-          delete nativeProps.fs;
-        };
-      };
-    } else {
-      /*
-       * For compatibility issues with an older version where setting both
-       * `playsInline` and `videoId` in quick succession would cause the video
-       * to sometimes not play.
-       */
-      delete nativeProps.playsInline;
+        return <RCTYouTube {... nativeProps} />;
     }
-
-    return <RCTYouTube {... nativeProps} />;
-  }
 }
 
 const styles = StyleSheet.create({
-  base: {
-    overflow: 'hidden',
-  },
+    base: {
+        overflow: 'hidden',
+    },
 });
