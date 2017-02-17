d0ce004c22e5d720610aba4aefbff6f80bbd04ff
William Candillon <wcandillon@gmail.com>
2017-02-17 13:57:04 +0100

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+declare module "react-native-youtube" {
+
+    interface Event {
+        target: number;
+    }
+
+    export interface State extends Event {
+        state: "buffering" | "playing" | "paused" | "ended";
+    }
+
+    export interface Progress extends Event {
+        duration: number;
+        currentTime: number;
+    }
+
+    export interface Error extends Event {
+        error: "invalid_param" | "html5_error" | "video_not_found" | "not_embeddable" | "unknown";
+    }
+
+    export interface Quality extends Event {
+        quality: "small" | "medium" | "large" | "hd720" | "hd1080" | "high_res" | "auto" | "default" | "unknown";
+    }
+
+    export interface YoutubeProps {
+        videoId: string;
+        play?: boolean;
+        hidden?: boolean;
+        playsInline?: boolean;
+        loop?: boolean;
+        modestbranding?: boolean;
+        fs?: boolean;
+        rel? :boolean;
+        showinfo?: boolean;
+        onRead?: () => void;
+        onChangeState?: (event: State) => void;
+        onChangeQuality?: (event: Quality) => void;
+        onError?: (event: Error) => void;
+        onProgress?: (event: Progress) => void;
+        onFullScreenExit?: () => void;
+        onFullScreenEnter?: () => void;
+        style?: React.ViewStyle
+        ref?: React.Ref<React.WebViewStatic & React.ViewStatic>
+    }
+    export default class Youtube extends React.Component<YoutubeProps, any>{}
+}
