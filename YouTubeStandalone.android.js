e9ba921448a39d6127659a98353bc83768e5e072
Jonathan Tafel <tafelito@gmail.com>
2017-07-11 00:14:56 -0400

    Fix standalone player export method

--- YouTubeStandalone.android.js
+++ YouTubeStandalone.android.js
@@ -6,13 +6,13 @@ const { YouTubeStandaloneModule } = NativeModules;
 export const YouTubeStandaloneAndroid = !YouTubeStandaloneModule
   ? null
   : {
-      playVideo: ({
+      playVideo: (
         apiKey,
         videoId,
         autoplay = false,
         lightboxMode = false,
         startTime = 0,
-      }) =>
+      ) =>
         new Promise((resolve, reject) =>
           YouTubeStandaloneModule.playVideo(
             apiKey,
