1b2481f876113016151638b7564a2c4af2da2ba5
Thanakij Pechprasarn <pechpras@gmail.com>
2019-01-26 13:19:58 +0700

    Fix a crash issue 'Fatal Exception: java.lang.IllegalStateException. Can not perform this action after onSaveInstanceState (YouTubeView.java:50)'

--- android/src/main/java/com/inprogress/reactnativeyoutube/YouTubeView.java
+++ android/src/main/java/com/inprogress/reactnativeyoutube/YouTubeView.java
@@ -47,7 +47,7 @@ public class YouTubeView extends FrameLayout {
     protected void onAttachedToWindow() {
         if (!mHasSavedInstance) {
             FragmentManager fragmentManager = getReactContext().getCurrentActivity().getFragmentManager();
-            fragmentManager.beginTransaction().add(getId(), mVideoFragment).commit();
+            fragmentManager.beginTransaction().add(getId(), mVideoFragment).commitAllowingStateLoss();
         }
         super.onAttachedToWindow();
     }
