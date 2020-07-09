58011e29f233188d27e4353a36f5727d3f37c83d
Pragnesh <30809418+pragneshpj@users.noreply.github.com>
2020-07-09 17:40:07 +0530

    added apiKey in YouTubeProps interface
    it shows compile error.

--- main.d.ts
+++ main.d.ts
@@ -13,6 +13,7 @@ export interface YouTubeProps {
   modestbranding?: boolean;
   showFullscreenButton?: boolean;
   rel?: boolean;
+  apiKey?: string;
   origin?: string;
   onError?: (event: any) => void;
   onReady?: (event: any) => void;
