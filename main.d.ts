d8602168e013e1eb2750604e25d37c358e458231
Pankaj Pandey <pnkajpande@gmail.com>
2020-08-06 16:48:59 +0530

    Update main.d.ts
    apiKey is missing in types. Its a mandatory prop.

--- main.d.ts
+++ main.d.ts
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
 
 export interface YouTubeProps {
+  apiKey: string;
   videoId?: string;
   videoIds?: string[];
   playlistId?: string;
